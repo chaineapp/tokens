ff9f29390b461bd7d7580f898c629520aa8c1543
Annalise Davis <annalise.davis@icloud.com>
2021-01-19 22:46:58 -0800

    Index files

---
+++ index.js
@@ -0,0 +1,6 @@
+import {colors} from "./tokens/colors";
+import {spacing} from "./tokens/spacing";
+import {typography} from "./tokens/typography";
+import {theme} from "./theme";
+
+export {theme, colors, spacing, typography};
---
+++ theme/index.js
@@ -0,0 +1,10 @@
+import {colors} from "./colors";
+import {spacing} from "./spacing";
+import {typography} from "./typography";
+
+export const theme = {
+  colors,
+  space: spacing,
+  fontSizes: typography.fontSizes,
+  lineHeights: typography.lineHeights
+};
