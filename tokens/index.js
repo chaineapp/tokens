bfe55dda97d936006275e05a7629f9829de7ba69
Annalise Davis <annalise.davis@icloud.com>
2021-01-19 22:32:19 -0800

    First commit

---
+++ tokens/index.js
@@ -0,0 +1,10 @@
+import {colors} from "../primitives/colors";
+import {spacing} from "../primitives/spacing";
+import {typography} from "../primitives/typography";
+
+export const theme = {
+  colors,
+  space: spacing,
+  fontSizes: typography.fontSizes,
+  lineHeights: typography.lineHeights
+};
