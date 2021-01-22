83f0ff30f143bfacad7aa11b7dcef31211fe6451
Annalise Davis <annalise.davis@icloud.com>
2021-01-21 18:21:07 -0800

    Fixed import error and added values to the exported theme

--- theme/index.js
+++ theme/index.js
@@ -1,10 +1,12 @@
-import {colors} from "./colors";
+import {colors} from "../tokens/colors";
 import {spacing} from "../tokens/spacing";
 import {typography} from "../tokens/typography";
 
 export const theme = {
   colors,
   space: spacing,
+  fonts: typography.fonts,
+  fontsWeights: typography.fontsWeights,
   fontSizes: typography.fontSizes,
   lineHeights: typography.lineHeights
 };
