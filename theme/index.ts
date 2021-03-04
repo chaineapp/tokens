e58e8635654e2a0c5a9e24293ab6085d31415b1b
Annalise Davis <60056818+shesupplypi@users.noreply.github.com>
2021-03-04 13:55:01 -0800

    changed to ts and most likely introduced breaking changes

---
+++ theme/index.ts
@@ -0,0 +1,12 @@
+import { colors } from '../tokens/colors'
+import { spacing } from '../tokens/spacing'
+import { typography } from '../tokens/typography'
+
+export const theme = {
+    colors,
+    space: spacing,
+    fonts: typography.fonts,
+    fontsWeights: typography.fontWeights,
+    fontSizes: typography.fontSizes,
+    lineHeights: typography.lineHeights,
+}
