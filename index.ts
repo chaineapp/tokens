5d7b358381232451b895affe3b6de206ae3718e2
shesupplypi <60056818+shesupplypi@users.noreply.github.com>
2021-07-18 20:12:41 -0700

    fixed error

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import {colors} from "./tokens/colors";
-import {colorThemes} from "./tokens/colorThemes";
 import {spacing} from "./tokens/spacing";
 import {typography} from "./tokens/typography";
 import {theme} from "./theme";
 
-export {theme, colors, colorThemes, spacing, typography};
+export {theme, colors, spacing, typography};
